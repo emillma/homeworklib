75da02d78d7683907558709175050e21ca5c1a96
Emil Martens <emil.martens@gmail.com>
2022-08-19 16:05:35 +0000

    inteligent datacatching

--- hwlib/keywords/datacatcher.py
+++ hwlib/keywords/datacatcher.py
@@ -5,11 +5,29 @@ import inspect
 import __main__
 
 
+def getidx(n, space):
+    """
+    Used to distribute data efficiently
+    """
+    step = 1 << (n//space).bit_length()
+    if n < space:
+        return n
+    elif n % step != 0:
+        return None
+    number = n//step - (space+1)//2
+    replacing = step//2 + number*step
+    return getidx(replacing, space)
+
+
 class DataCatcher:
     def __init__(self, maxdata):
         self.data = {}
         self.maxdata = maxdata
         self.data_is_saved = False
+
+        self.step = 1
+        self.n = 0
+        self.idx = 0
 
     def set_maxdata(self, maxdata):
         self.maxdata = maxdata
@@ -28,10 +46,16 @@ class DataCatcher:
             fdata = self.data.setdefault(fkey, [])
             if len(fdata) < self.maxdata:
                 fdata.append((argvals, ret))
-            else:
-                raise Exception
+            elif idx := getidx(self.n, self.maxdata):
+                fdata[idx] = (argvals, ret)
+
+            self.n += 1
             return ret
         return wrapper
+
+    def set_data(self, pos):
+        if self.n % self.step != 0:
+            return
 
     def dump(self, fpath: Path):
         with open(fpath, 'wb') as file:
